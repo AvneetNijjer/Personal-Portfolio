986e5b4b06803d1378b8216e92e892d8117e8e60
Avneet Singh Nijjer <137588665+AvneetNijjer@users.noreply.github.com>
2025-02-06 14:35:07 -0500

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -220,7 +220,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="Avneet Resume.pdf" target="_blank" className="w-full lg:w-auto">
+              <a href="AvneetSinghNijjer Resume.pdf" target="_blank" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
